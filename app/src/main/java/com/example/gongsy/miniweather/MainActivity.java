e78b2a87c46bc59227ede1b683a6f2eb41245152
shaoyugong <1114730486@qq.com>
2017-09-27 10:46:27 +0800

    init project

---
+++ app/src/main/java/com/example/gongsy/miniweather/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.gongsy.miniweather;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Gongsy on 2017/9/27.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+
+}
